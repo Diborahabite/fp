610b20561a8df0da033978491cde1362e8ea9768
diboradiborah <123638382+diboradiborah@users.noreply.github.com>
2024-05-07 23:44:13 +0300

    commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import DoctorDetail from './DoctorDetail';
+// Importing the DoctorDetail component
+
+const App = () => {
+  return (
+    <div>
+      {/* Render the DoctorDetail component */}
+      <DoctorDetail  />
+    </div>
+  );
+};
+
+export default App;
+
